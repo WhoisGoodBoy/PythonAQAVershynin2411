2b2dd1b9e8a7bb999f8ed8320db4b69161c60771
WhoisGoodBoy <veolvl@gmail.com>
2024-01-30 21:17:46 +0200

    added marks

--- lesson18/tests/test_product.py
+++ lesson18/tests/test_product.py
@@ -1,12 +1,13 @@
+import pytest
 
 
-
+@pytest.mark.regression
 def test_check_header(product):
     header = product.wait_until_element_appears(product.locator_header)
     assert header.text == 'ЖАХ АРКХЕМУ. ТРЕТЯ РЕДАКЦІЯ (УКР.)'
     assert product.driver.current_url == 'https://nastolka.com.ua/uk/uzhas-arkhema-tretya-redakciya'
 
-
+@pytest.mark.regression
 def test_check_header_with_diff_driver(product2):
     header = product2.wait_until_element_appears(product2.locator_header)
     assert header.text == 'ЖАХ АРКХЕМУ. ТРЕТЯ РЕДАКЦІЯ (УКР.)'
--- lesson25/test_cat.py
+++ lesson25/test_cat.py
@@ -1,8 +1,8 @@
-
+import pytest
 from lesson25 import infrastructure as infra
 
 
-
+@pytest.mark.regression
 def test_cat_creation():
     list_of_cats_actual,  list_of_cats_expected = infra.filter_cats_and_only_cats()
     for cat in list_of_cats_expected:
